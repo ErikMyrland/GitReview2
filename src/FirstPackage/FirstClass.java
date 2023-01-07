05242dfe13bb01e83b23e9810651ddfdc3ab89d8
ErikMyrland <114176328+ErikMyrland@users.noreply.github.com>
2023-01-07 13:33:07 -0600

    Update FirstClass.java

--- src/FirstPackage/FirstClass.java
+++ src/FirstPackage/FirstClass.java
@@ -4,5 +4,10 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("First class");
+        
+        // Some changes
+        
+        System.out.println("First class");
+
     }
 }
